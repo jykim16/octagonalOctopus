d413f12f8bef4a78859b5c617e815158071be62a
Jonathan Kim <jykim16@gmail.com>
2017-08-24 13:02:42 -0700

    added background to gameboard

--- react-client/src/components/GameBoard/VoteTrack.jsx
+++ react-client/src/components/GameBoard/VoteTrack.jsx
@@ -4,8 +4,13 @@ import React from 'react';
 let VoteTrack = (props) => {
 
   return (
-    <div>
-
+    <div className='votetrack'>
+      <div>Vote Track</div>
+      <span className={`circle ${props.voteTrack[0] ? 'pass' : 'fail'} ${props.voteTrack.length >= 0 ? 'active' : 'inactive'}`}></span>
+      <span className={`circle ${props.voteTrack[1] ? 'pass' : 'fail'} ${props.voteTrack.length >= 1 ? 'active' : 'inactive'}`}></span>
+      <span className={`circle ${props.voteTrack[2] ? 'pass' : 'fail'} ${props.voteTrack.length >= 2 ? 'active' : 'inactive'}`}></span>
+      <span className={`circle ${props.voteTrack[3] ? 'pass' : 'fail'} ${props.voteTrack.length >= 3 ? 'active' : 'inactive'}`}></span>
+      <span className={`circle ${props.voteTrack[4] ? 'pass' : 'fail'} ${props.voteTrack.length >= 4 ? 'active' : 'inactive'}`}></span>
     </div>
   );
 }
@@ -14,5 +19,5 @@ export default VoteTrack;
 
 VoteTrack.propTypes = {
   isHost: PropTypes.func,
-  questNumber: PropTypes.number
+  voteTrack: PropTypes.array
 }
